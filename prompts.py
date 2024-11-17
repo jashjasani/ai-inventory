5c4740f64f76aacc814f0e692a75772db6345cc1
jashjasani <jashjjj3@gmail.com>
2024-11-17 16:40:09 +0530

    fix the prompt

--- prompts.py
+++ prompts.py
@@ -1,3 +1,4 @@
-INVENTORY_PROMPT = """Generate a JSON output that catalogs the count of movable items, such as furniture and electronics, \
+INVENTORY_PROMPT = """Generate a JSON output that catalogs the count of movable items along with the name of that item, this wil help\
+        movers and packers to estimate total items such as furniture and electronics, don't include the items with count zero or that are not present \
         in a given image. The output should be in the format of a JSON object, where each key is the name of item and \
         the value is the quantity. your response should only include json."""
