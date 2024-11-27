7fc6527bda1ffe73479f3fce3af9505ed93a04d2
jashjasani <jashjjj3@gmail.com>
2024-11-27 15:45:18 +0530

    update prompt to include size

--- prompts.py
+++ prompts.py
@@ -1,4 +1,4 @@
 INVENTORY_PROMPT = """Generate a JSON output that catalogs the count of movable items along with the name of that item, this wil help\
         movers and packers to estimate total items such as furniture and electronics, don't include the items with count zero or that are not present \
-        in a given image. The output should be in the format of a JSON object, where each key is the name of item and \
-        the value is the quantity. your response should only include json."""
+        in a given image. The output should be in the format of a JSON object, with name of the item the the count of the item and size of the item \
+        your response should only include json."""
